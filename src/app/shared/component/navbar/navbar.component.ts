af2b84e0f3c835b7e6149dff682aab4f269e4f40
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-11-22 23:33:34 -0500

    feat: Updated navbar

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -9,8 +9,8 @@ export class NavbarComponent implements OnInit{
   items = [
     { label: 'Home', icon: 'pi pi-home', routerLink: '/supplier-home' },
     { label: 'Add Shipment', icon: 'pi pi-send', routerLink: '/add-shipment' },
-    { label: 'Notifications', icon: 'pi pi-bell', routerLink: '' },
-    { label: 'Inventory', icon: 'pi pi-shopping-cart', routerLink: '/supplier-inventory' },
+    //{ label: 'Notifications', icon: 'pi pi-bell', routerLink: '' },
+    //{ label: 'Inventory', icon: 'pi pi-shopping-cart', routerLink: '/supplier-inventory' },
     { label: 'Profile', icon: 'pi pi-user', routerLink: '/supplier-profile' }
   ];
 
